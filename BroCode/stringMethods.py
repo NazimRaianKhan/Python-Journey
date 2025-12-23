c30e0a4155e37d0b6973f9a78db2aec0aca8e1b8
Nazim Raian Khan <nazimraiank@gmail.com>
2025-12-23 23:27:46 +0600

    string methods assignment

--- BroCode/stringMethods.py
+++ BroCode/stringMethods.py
@@ -30,5 +30,12 @@ print(help(str))
 # 2. username must not contain spaces
 # 3. username must not contain digits
 
+username = input("Enter a username: ")
 
+if len(username) > 12:
+    print("Your username cant be more than 12 char")
+elif not username.find(" ") == -1:\
+    print("Your username cant contain spaces")
+elif not username.isalpha():
+    print("Your username cant contain numbers")
 
